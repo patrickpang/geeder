7b124587141f1dab2f0f0df88de6a69dc5ebbd61
Patrick Pang <imptrik@gmail.com>
2024-05-01 14:59:31 +0000

    Add some htmx in

--- web/main.py
+++ web/main.py
@@ -1,5 +1,17 @@
 import dominate
-from dominate.tags import body, footer, form, h1, header, input_, main, p, textarea
+from dominate.tags import (
+    body,
+    div,
+    footer,
+    form,
+    h1,
+    header,
+    input_,
+    main,
+    p,
+    script,
+    textarea,
+)
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
@@ -7,7 +19,7 @@ app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
-async def home_route():
+async def home_page() -> str:
     doc = dominate.document(title="Geeder")
 
     with doc:
@@ -15,13 +27,29 @@ async def home_route():
             with header():
                 h1("Geeder")
                 p("Your study copilot with Anki cards")
+
             with main():
-                with form():
+                # input
+                with form(**{"hx-post": "/generate", "hx-target": "#output-message"}):
                     textarea(
                         name="excerpt", placeholder="Enter excerpt from textbook here"
                     )
                     input_(type="submit", value="Submit")
+                # output
+                div(id="output-message")
+
             with footer():
                 p("Made with ❤️ by Patrick")
 
+            script(src="https://unpkg.com/htmx.org@1.9.12")
+
     return doc.render()
+
+
+@app.post("/generate", response_class=HTMLResponse)
+async def generate_segment() -> str:
+    segment = div()
+    with segment:
+        p("Generating Anki cards...")
+        p("Anki cards generated!")
+    return segment.render()
