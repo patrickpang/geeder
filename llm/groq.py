a45d0ac59c440fe56bff3d9343a52a6a82d477cb
Patrick Pang <imptrik@gmail.com>
2024-05-03 14:44:34 +0000

    Polish UI

--- llm/groq.py
+++ llm/groq.py
@@ -1,10 +1,11 @@
 from typing import AsyncIterator
+
 from groq import AsyncGroq
 
 client = AsyncGroq()
 
 MODEL = "llama3-70b-8192"
-PROMPT = "Create multiple Anki cards from below text. Return front and back on separate lines."
+PROMPT = "Create multiple Anki cards from below text. Question on front. Answer on back, Return front and back on separate lines."
 
 
 async def get_cards(excerpt: str) -> str:
@@ -23,6 +24,7 @@ async def get_cards(excerpt: str) -> str:
     )
 
     return response.choices[0].message.content
+
 
 async def get_cards_streaming(excerpt: str) -> AsyncIterator[str]:
     async for chunk in await client.chat.completions.create(
--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from typing import Annotated
 import dominate
 from dominate.tags import (
     body,
+    button,
     div,
     footer,
     form,
     h1,
+    head,
     header,
-    input_,
+    link,
     main,
     p,
-    pre,
     script,
+    span,
+    style,
     textarea,
 )
 from fastapi import FastAPI, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, PlainTextResponse
 
 from llm.groq import get_cards
 
@@ -28,34 +31,66 @@ async def home_page() -> str:
     doc = dominate.document(title="Geeder")
 
     with doc:
-        with body():
-            with header():
-                h1("Geeder")
+        with head():
+            link(
+                href="https://cdn.jsdelivr.net/npm/daisyui@4.10.5/dist/full.min.css",
+                rel="stylesheet",
+                type="text/css",
+            )
+            script(src="https://cdn.tailwindcss.com")
+
+        with body(_class="mx-16 lg:mx-64 mt-16"):
+            with header(_class="mb-8"):
+                h1("Geeder", _class="text-4xl font-bold mb-2")
                 p("Your study copilot with Anki cards")
 
             with main():
                 # input
                 with form(**{"hx-post": "/generate", "hx-target": "#output-message"}):
                     textarea(
-                        name="excerpt", placeholder="Enter excerpt from textbook here"
+                        name="excerpt",
+                        placeholder="Enter excerpt from textbook here",
+                        _class="textarea textarea-bordered block w-full mb-4",
+                        rows=5,
+                        required=True,
+                        minlength=10,
                     )
-                    input_(type="submit", value="Submit")
+                    with div(_class="text-right"):
+                        with button(type="submit", _class="btn block"):
+                            style(
+                                """
+                                .loading-indicator{
+                                    display:none;
+                                }
+                                .htmx-request .loading-indicator{
+                                    display:inline;
+                                }
+                                .htmx-request.loading-indicator{
+                                    display:inline;
+                                }
+                                """
+                            )
+                            span(
+                                _class="loading loading-dots loading-md loading-indicator"
+                            )
+                            span("Submit")
                 # output
-                div(id="output-message")
+                div(
+                    "Response will appear here",
+                    id="output-message",
+                    _class="whitespace-pre-wrap alert mt-8",
+                    style="padding: 1.5rem",  # override .alert from daisyUI
+                )
 
-            with footer():
-                p("Made with ❤️ by Patrick")
+            with footer(_class="mt-8"):
+                p("Made with ❤️ by Patrick", _class="text-sm")
 
             script(src="https://unpkg.com/htmx.org@1.9.12")
 
     return doc.render()
 
 
-@app.post("/generate", response_class=HTMLResponse)
-async def generate_segment(excerpt: Annotated[str, Form()]) -> str:
+@app.post("/generate", response_class=PlainTextResponse)
+async def generate_text(excerpt: Annotated[str, Form()]) -> str:
     response = await get_cards(excerpt)
-
-    segment = pre()
-    with segment:
-        p(response)
-    return segment.render()
+    return response
