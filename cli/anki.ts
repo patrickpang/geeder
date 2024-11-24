0cad92355b1e905548bc0db40a605ef698266fc3
Patrick Pang <imptrik@gmail.com>
2024-11-24 08:17:12 +0000

    Remove unused methods

--- cli/anki.ts
+++ cli/anki.ts
@@ -1,22 +1,6 @@
 import { ankiConnectCallRpc } from "./anki_connect.ts";
 import { Card } from "./model.ts";
 
-export async function addCard(
-  deck: string,
-  question: string,
-  answer: string,
-): Promise<void> {
-  const note = {
-    "deckName": deck,
-    "modelName": "Basic",
-    "fields": { "Front": question, "Back": answer },
-    "tags": ["geeder"],
-  };
-  console.info("addCard start");
-  const result = await ankiConnectCallRpc("addNotes", { notes: [note] });
-  console.info("addCard end", { result });
-}
-
 export async function addCards(deck: string, cards: Card[]): Promise<void> {
   const notes = cards.map((card) => ({
     "deckName": deck,
